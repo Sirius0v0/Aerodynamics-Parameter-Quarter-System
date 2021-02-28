cd54ef924430336943429e871d02ed4d5a53c648
Sirius0v0 <1694605269@qq.com>
2021-02-28 21:15:17 +0800

    修复斜激波界面崩溃

--- Function/callbackFunc/callback_buttondown.m
+++ Function/callbackFunc/callback_buttondown.m
@@ -3,12 +3,13 @@ function callback_buttondown(~,~,h_fig)
 %% 获得数据
 data = guidata(h_fig);
 fig_point = get(gcbf,'CurrentPoint');   % 获得鼠标在窗口坐标中的位置
+pum = getappdata(h_fig,'pum');
 gcaa = gca;     % 获取当前坐标进行操作
 point = get(gcaa,'CurrentPoint');       % 获得鼠标在画布坐标上的位置
 switch (get(gcbf,'SelectionType'))
     case 'extend'   % Shift+左键（或滚轮键）
-        if data.NormalGraphExist || data.ObliqueGraphExist
-            % 如果画了图再判断
+        if (data.NormalGraphExist && (pum.Value==1) ) || (data.ObliqueGraphExist && (pum.Value==2) )
+            % 如果对应的界面画了图再判断
             if (fig_point(1)<0.66 && fig_point(1)>0.06)&&(fig_point(2)<0.83 && fig_point(2)>0.08)
                % 根据图框大小判断鼠标是否在图框内
                dist = ((point(1,1)-gcaa.Children(end).XData).^2 + (point(1,2)-gcaa.Children(end).YData).^2).^.5;  % 计算与线条的距离
--- Function/callbackFunc/callback_motion.m
+++ Function/callbackFunc/callback_motion.m
@@ -2,12 +2,13 @@ function callback_motion(~,~,h_fig)
 % 改变鼠标形状
 %% 获取数据
 data = guidata(h_fig);
+pum = getappdata(h_fig,'pum');
 gcaa = gca;     % 获取当前图窗句柄
 point = get(gcaa,'CurrentPoint');   % 获取鼠标当前坐标
 
 %% 设置鼠标形状
-if data.NormalGraphExist || data.ObliqueGraphExist
-    % 如果画了图则启用
+if (data.NormalGraphExist && (pum.Value==1) ) || (data.ObliqueGraphExist && (pum.Value==2) )
+    % 如果对应的界面画了图则启用
     if min(((point(1,1)-gcaa.Children(end).XData).^2 + (point(1,2)-gcaa.Children(end).YData).^2).^.5) < ...
             0.05 * ( min(gcaa.XLim(2)-gcaa.XLim(1), gcaa.YLim(2)-gcaa.YLim(1)) )
         % 当靠近线条 则
